c0fd14302dda740b6d5793e5a8c91ae58710bc16
Anton G <iskinn@inbox.ru>
2018-10-15 00:26:25 +0300

    Lesson_04

--- lesson04/home_work/hw04_normal.py
+++ lesson04/home_work/hw04_normal.py
@@ -4,6 +4,12 @@
 # Т.е. из строки "mtMmEZUOmcq" нужно получить ['mt', 'm', 'mcq']
 # Решить задачу двумя способами: с помощью re и без.
 
+###################
+# Способ 1        #
+###################
+
+import re
+
 line = 'mtMmEZUOmcqWiryMQhhTxqKdSTKCYEJlEZCsGAMkgAYEOmHBSQsSUHKvSfbmxULaysmNO'\
        'GIPHpEMujalpPLNzRWXfwHQqwksrFeipEUlTLeclMwAoktKlfUBJHPsnawvjPhfgewVzK'\
        'TUfSYtBydXaVIpxWjNKgXANvIoumesCSSvjEGRJosUfuhRRDUuTQwLlJJJDdkVjfSAHqn'\
@@ -20,6 +26,33 @@ line = 'mtMmEZUOmcqWiryMQhhTxqKdSTKCYEJlEZCsGAMkgAYEOmHBSQsSUHKvSfbmxULaysmNO'\
        'XiUWgsKQrDOeZoNlZNRvHnLgCmysUeKnVJXPFIzvdDyleXylnKBfLCjLHntltignbQoiQ'\
        'zTYwZAiRwycdlHfyHNGmkNqSwXUrxGc'
 
+pattern = '[A-Z]+([a-z]+)+[A-Z]'
+
+print(re.findall(pattern, line))
+
+######################
+# Способ 2. Школьный #
+######################
+
+alfavit_lower = 'abcdefghijklmnopqrstuvwxyz'
+
+new_list = []
+new_list_element = ''
+up_count = 0
+
+for i in line:
+    if alfavit_lower.count(i) > 0:
+        new_list_element += i
+        up_count = 0
+    else:
+        if up_count == 0:
+            new_list.append(new_list_element)
+            up_count += 1
+            new_list_element = ''
+        else:
+            pass
+
+print(new_list)
 
 # Задание-2:
 # Вывести символы в верхнем регистре, слева от которых находятся
@@ -45,9 +78,56 @@ line_2 = 'mtMmEZUOmcqWiryMQhhTxqKdSTKCYEJlEZCsGAMkgAYEOmHBSQsSUHKvSfbmxULaysm'\
        'JFaXiUWgsKQrDOeZoNlZNRvHnLgCmysUeKnVJXPFIzvdDyleXylnKBfLCjLHntltignbQ'\
        'oiQzTYwZAiRwycdlHfyHNGmkNqSwXUrxGC'
 
+###################
+# Способ 1        #
+###################
+
+import re
+
+pattern = '[a-z]{2}([A-Z])[A-Z]{2}'
+
+print(re.findall(pattern, line))
+
+###################
+# Способ 2        #
+###################
+
+alfavit_up = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ'
+
+for index in range(len(line)):
+    if alfavit_up.count(line[index]) == 1 and alfavit_up.count(line[index-1]) == 0 and alfavit_up.count(line[index-2]) == 0 and alfavit_up.count(line[index+1]) == 1 and alfavit_up.count(line[index+1]) == 1:
+        print(line[index])
+
 # Задание-3:
 # Напишите скрипт, заполняющий указанный файл (самостоятельно задайте имя файла)
 # произвольными целыми цифрами, в результате в файле должно быть
 # 2500-значное произвольное число.
 # Найдите и выведите самую длинную последовательность одинаковых цифр
 # в вышезаполненном файле.
+
+import random
+import re
+
+# Заполняем
+f = open('file.txt', 'w')
+
+for el in range(2500):
+    f.write(str(random.randint(0, 9)))
+
+f = open('file.txt', 'r')
+
+# Ищем последовательности
+a = f.read()
+c = []
+
+# Формируем список с последвательными цифрами
+for i in range(10):
+    c += re.findall(r'{0}{0}+'.format(i), a)
+
+# Ищем длину самого длинного элемента списка
+max_element_len = len(max(c, key=len))
+
+# Выводим все элементы с максимальной длиной. Их може быть несколько.
+print([i for i in c if len(i) == max_element_len])
+
+f.close()
---
+++ lesson05/home_work/l_00.py
@@ -0,0 +1,16 @@
+import sys
+
+#print("path - ", sys.argv)
+
+#print("modules - ", sys.path)
+
+#print("interpret - ", sys.executable)
+
+#print("Загруженные - ", sys.modules)
+
+#print("OC - ", sys.platform)
+
+while True:
+    key = input("Нажмите q для выхода")
+    if key == 'q':
+        sys.exit()
